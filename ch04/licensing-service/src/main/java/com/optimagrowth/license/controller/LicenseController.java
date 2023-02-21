bea3d9b8255a543f365668ace2ee3a7c901b7770
sbrace <sbrace0822@gmail.com>
2023-02-21 18:50:31 +0800

    ch04 licensing-service init

---
+++ ch04/licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -0,0 +1,52 @@
+package com.optimagrowth.license.controller;
+
+import com.optimagrowth.license.model.License;
+import com.optimagrowth.license.service.LicenseService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
+
+@RestController
+@RequestMapping("v1/organization/{organizationId}/license")
+public class LicenseController {
+
+
+    private final LicenseService licenseService;
+
+    public LicenseController(LicenseService licenseService) {
+        this.licenseService = licenseService;
+    }
+
+    @GetMapping("/{licenseId}")
+    public ResponseEntity<License> getLicense(
+            @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId) {
+        License license = licenseService.getLicense(licenseId, organizationId);
+        return ResponseEntity.ok(license);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateLicense(
+            @PathVariable("organizationId") String organizationId,
+            @RequestBody License license) {
+        return ResponseEntity.ok(licenseService.updateLicense(license, organizationId));
+    }
+
+
+    @PostMapping
+    public ResponseEntity<String> createLicense(
+            @PathVariable("organizationId") String organizationId,
+            @RequestBody License license,
+            @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
+        return ResponseEntity.ok(licenseService.createLicense(license, organizationId, locale));
+    }
+
+    @DeleteMapping("/{licenseId}")
+    public ResponseEntity<String> deleteLicense(
+            @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId) {
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId));
+    }
+
+}
