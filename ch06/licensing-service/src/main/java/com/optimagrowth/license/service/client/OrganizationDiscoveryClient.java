d902429411e421b1f08c6bdfa1de83a24d74701a
sbrace <sbrace0822@gmail.com>
2023-03-04 07:23:25 +0800

    ch06 licensing-service remove @Autowaired

--- ch06/licensing-service/src/main/java/com/optimagrowth/license/service/client/OrganizationDiscoveryClient.java
+++ ch06/licensing-service/src/main/java/com/optimagrowth/license/service/client/OrganizationDiscoveryClient.java
@@ -1,8 +1,6 @@
 package com.optimagrowth.license.service.client;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import com.optimagrowth.license.model.Organization;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
@@ -10,13 +8,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import com.optimagrowth.license.model.Organization;
+import java.util.List;
 
 @Component
 public class OrganizationDiscoveryClient {
 
-    @Autowired
-    private DiscoveryClient discoveryClient;
+    private final DiscoveryClient discoveryClient;
+
+    public OrganizationDiscoveryClient(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
 
     public Organization getOrganization(String organizationId) {
         RestTemplate restTemplate = new RestTemplate();
--- ch06/licensing-service/src/main/java/com/optimagrowth/license/service/client/OrganizationRestTemplateClient.java
+++ ch06/licensing-service/src/main/java/com/optimagrowth/license/service/client/OrganizationRestTemplateClient.java
@@ -1,17 +1,19 @@
 package com.optimagrowth.license.service.client;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.optimagrowth.license.model.Organization;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import com.optimagrowth.license.model.Organization;
-
 @Component
 public class OrganizationRestTemplateClient {
-    @Autowired
-    RestTemplate restTemplate;
+
+    private final RestTemplate restTemplate;
+
+    public OrganizationRestTemplateClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public Organization getOrganization(String organizationId){
         ResponseEntity<Organization> restExchange =
